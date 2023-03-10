e29ee8cf7c9dafd03630af432d900d8ba2ac3e8f
Maciej Dresler <alinoe@protonmail.com>
2023-03-10 16:50:25 +0100

    Fixed OpanAPI/Swagger integration.

--- src/main/java/pl/xyz/cadmus/CadmusServerApplication.java
+++ src/main/java/pl/xyz/cadmus/CadmusServerApplication.java
@@ -2,12 +2,7 @@ package pl.xyz.cadmus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -15,14 +10,5 @@ public class CadmusServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CadmusServerApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
     }
 }
